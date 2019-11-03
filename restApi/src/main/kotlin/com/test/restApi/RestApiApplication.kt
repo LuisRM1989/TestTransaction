614bd6439d2619a8ef933ada2e949fabb22b5edb
Luis <ang_55rm_luis@hotmail.com>
2019-11-02 22:22:49 -0600

    Modificacion inicio

--- restApi/src/main/kotlin/com/test/restApi/RestApiApplication.kt
+++ restApi/src/main/kotlin/com/test/restApi/RestApiApplication.kt
@@ -1,22 +1,11 @@
 package com.test.restApi
 
-import com.test.restApi.dao.OwnerRepository
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-
 @SpringBootApplication
-class RestApiApplication: CommandLineRunner{
+class RestApiApplication
 
-	@Autowired
-	val ownerRepository: OwnerRepository? = null
-	override fun run(vararg args: String?) {
-
-	}
-
-}
 fun main(args: Array<String>) {
 	runApplication<RestApiApplication>(*args)
 }
